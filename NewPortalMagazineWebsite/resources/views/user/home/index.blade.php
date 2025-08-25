04e49f3a88fa2dca1db4257fcc6c0d6cc2172be9
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-08-25 22:52:39 +0700

    Add homepage sections and layout components
    Introduced new Blade templates for the homepage, including trending news carousel, popular news, and popular news category sections. Added header and footer layout components, and updated the master layout and homepage to include these new sections for improved structure and content organization.

--- NewPortalMagazineWebsite/resources/views/user/home/index.blade.php
+++ NewPortalMagazineWebsite/resources/views/user/home/index.blade.php
@@ -1,3 +1,29 @@
 @extends('user.layouts.master')
 
 @section('title', 'Home')
+
+@section('content')
+<!-- Tranding news carousel-->
+@include('user.home.tranding-new-carousel')
+<!-- End Tranding news carousel -->
+
+<!-- Popular news -->
+@include('user.home.popular-news')
+<!-- End Popular news -->
+
+<div class="large_add_banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="large_add_banner_img">
+                    <img src="{{ asset('user/images/placeholder_large.jpg') }}" alt="adds">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Popular news category -->
+@include('user.home.popular-news-category')
+<!-- End Popular news category -->
+@endsection
