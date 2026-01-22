2c82a9e200d97bedec3227dd08c60a94aa48b1a7
Hung Nguyen <hungthinh291@gmail.com>
2026-01-22 08:23:36 +0700

    dashboard admin UI

---
+++ laravel-project/resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,1 @@
+<p>Copyright © {{ date('Y') }} Hưng Nguyễn.</p>
---
+++ laravel-project/resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,11 @@
+<ul>
+    <li class="menu-title">
+        <span>Main Menu</span>
+    </li>
+    <li class="submenu active">
+        <a href="#"><i class="feather-grid"></i> <span> Dashboard</span></a>
+    </li>
+    <li class="submenu">
+        <a href="#"><i class="fas fa-users"></i> <span> Users</span></a>
+    </li>
+</ul>
